98be6b2b47c818abea3d3d3b05170a4fb349315e
jwchan1996 <741755613@qq.com>
2018-12-30 17:36:37 +0800

    add login && register

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,45 @@
 <template>
-    <h1>Login</h1>
+  <div class="login-box">
+    <form class="form-horizontal">
+      <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label">账号</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="inputEmail3" placeholder="">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">密码</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword3" placeholder="">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox"> 记住密码
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="submit" class="btn btn-default">登录</button>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .login-box{
+        width: 400px;
+        margin-top: 40px;
+    }
+</style>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,3 +1,42 @@
 <template>
-    <h1>Register</h1>
+  <div class="register-box">
+    <form class="form-horizontal">
+      <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label">账号</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="inputEmail3" placeholder="">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">密码</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword3" placeholder="">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputPassword3" class="col-sm-4 control-label">确认密码</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword3" placeholder="">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="submit" class="btn btn-default">注册</button>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .register-box{
+        width: 400px;
+        margin-top: 40px;
+    }
+</style>
